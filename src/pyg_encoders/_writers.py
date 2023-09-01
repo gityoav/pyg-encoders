4ef9e50cf04f58a771c98e6a5c5c1237ea02e7e2
Yoav Git <yoav.git@gmail.com>
2023-09-01 13:51:16 +0100

    pd_read_root

--- src/pyg_encoders/_writers.py
+++ src/pyg_encoders/_writers.py
@@ -1,13 +1,19 @@
-from pyg_encoders._encoders import csv_write, parquet_write, npy_write, pickle_write, _csv, _npy, _npa, _parquet, _pickle, root_path
+from pyg_encoders._encoders import csv_write, parquet_write, npy_write, pickle_write, _csv, _npy, _npa, _parquet, _pickle, _dictable, root_path
+from pyg_encoders._encoders import pickle_load, pd_read_csv, pd_read_npy, pd_read_parquet
 from pyg_encoders._encode import encode, decode 
-from pyg_base import passthru, is_str, as_list, get_cache, dt, getargspec, partialize
-
+from pyg_base import passthru, is_str, as_list, get_cache, dt, dictattr, getargspec, partialize, dictdir
+import os
 
 _WRITERS = 'WRITERS'
+_READERS = 'READERS'
 if _WRITERS not in get_cache():
     get_cache()[_WRITERS] = {}
+if _READERS not in get_cache():
+    get_cache()[_READERS] = {}
     
 WRITERS = get_cache()[_WRITERS]
+READERS = get_cache()[_READERS]
+
 WRITERS.update({_csv: csv_write , 
                _npy: partialize(npy_write, append = False), 
                '.np0': partialize(npy_write, append = False, max_workers = 0), 
@@ -16,6 +22,14 @@ WRITERS.update({_csv: csv_write ,
                '.parque0' : partialize(parquet_write, max_workers = 0),
                '.pickl0' : partialize(pickle_write, max_workers = 0),
                _pickle : pickle_write})
+
+READERS.update({_csv: pd_read_csv,
+                _pickle: pickle_load, 
+                _csv: pd_read_csv, 
+                _npy : pd_read_npy, 
+                _npa: pd_read_npy,
+                _parquet: pd_read_parquet
+                })
 
 def as_reader(reader = None):
     """
@@ -85,3 +99,94 @@ def as_writer(writer = None, kwargs = None, unchanged = None, unchanged_keys = N
     else:
         return as_list(writer)
 
+
+
+def _np_read_path(pth, ext, level = 0):
+    reader = READERS[ext]
+    if os.path.exists(pth) and os.path.exists(os.path.join(pth, 'data.npy')) and os.path.exists(os.path.join(pth, 'index.npy')):
+        return reader(pth + ext)
+    else:
+        return dictattr({k: _np_read_path(p, ext) for k,p in dictdir(pth, level = level).items()})/None
+
+def _pd_read_path(pth, ext, level = 0):
+    if 'np' in ext:
+        return _np_read_path(pth, ext, level)
+    reader = READERS[ext]
+    if os.path.exists(pth + ext):
+        return reader(pth + ext)
+    elif os.path.exists(pth):
+        return dictattr({k[:-len(ext)]: reader(v) for k, v in dictdir(pth, level = level).items() if k.endswith(ext)})/None
+        
+    
+
+def pd_read_root(root, doc = None, output = None, level = 0):
+    """
+    
+    Returns a list of dataframes 
+
+    Example: simple load of parquet file
+    --------
+    >>> from pyg import * 
+    >>> root = 'c:/temp/%x/%y.parquet'
+    >>> a = pd.Series([1,2,3], drange(2)); b = pd.DataFrame(dict(a = [1,2,3], b = 4), drange(2))
+    >>> doc = cell(add_, a = a, b = b, x = 'x', y = 'y', db = root).go()
+    >>> pd_read_root(root, cell(add_, a = a, b = b, x = 'x', y = 'y', db = root))
+
+    Example: simple load of npy file
+    --------
+    >>> from pyg import * 
+    >>> root = 'c:/temp/%x/%y.npy'
+    >>> a = pd.Series([1,2,3], drange(2)); b = pd.DataFrame(dict(a = [1,2,3], b = 4), drange(2))
+    >>> doc = cell(add_, a = a, b = b, x = 'x', y = 'npy', db = root).go()
+    >>> pd_read_root(root, cell(add_, a = a, b = b, x = 'x', y = 'npy', db = root))
+
+    Example: simple load of dict of files
+    --------
+    >>> from pyg import * 
+    >>> root = 'c:/temp/%x/%y.parquet'
+    >>> a = pd.Series([1,2,3], drange(2)); b = pd.DataFrame(dict(a = [1,2,3], b = 4), drange(2))
+    >>> f = lambda a,b: dict(add = add_(a,b), mul = mul_(a,b)); f.output = ['add', 'mul']
+    >>> doc = cell(f, a = a, b = b, x = 'x', y = 'parquet_dict', db = root).go()
+    >>> pd_read_root(root, cell(f, a = a, b = b, x = 'x', y = 'parquet_dict', db = root))
+
+    >>> f = lambda a,b: dict(add = add_(a,b), mul = mul_(a,b))
+    >>> doc = cell(f, a = a, b = b, x = 'x', y = 'parquet_dict_in_data', db = root).go()
+    >>> pd_read_root(root, cell(f, a = a, b = b, x = 'x', y = 'parquet_dict_in_data', db = root))
+
+    Example: simple load of dict of npy files
+    ---------
+    >>> from pyg import * 
+    >>> root = 'c:/temp/%x/%y.npy'
+    >>> a = pd.Series([1,2,3], drange(2)); b = pd.DataFrame(dict(a = [1,2,3], b = 4), drange(2))
+    >>> f = lambda a,b: dict(add = add_(a,b), mul = mul_(a,b))
+    >>> doc = cell(f, a = a, b = b, x = 'x', y = 'npy_dict_in_data', db = root).go()
+    >>> pd_read_root(root, cell(f, a = a, b = b, x = 'x', y = 'npy_dict_in_data', db = root))
+
+    
+    Parameters
+    ----------
+    root : str
+        Such as 'c:/temp/%x/%y.parquet'
+    doc : dict, optional
+        A document to populate the root keys from.
+    output : str/list, optional
+        list of keys we are interested to load from file
+
+    Returns
+    -------
+    dict of values
+        
+    """
+    doc = doc or {}
+    res = dictattr()
+    path = root_path(doc = doc, root = root)
+    if output is None:
+        output = as_list(getattr(doc, '_output', 'data'))
+
+    ext = '.' + root.split('.')[-1]
+    path = path[:-len(ext)]
+    for out in output:
+        if out not in doc:
+            pth = os.path.join(path, out)
+            res[out] = _pd_read_path(pth, ext, level)
+    return res / None
