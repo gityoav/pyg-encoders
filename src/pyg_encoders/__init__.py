148012521ba500bbdaf5981bd0c6a466eba53092
Yoav Git <30290427+gityoav@users.noreply.github.com>
2025-08-11 12:41:26 +0100

    dump and load

--- src/pyg_encoders/__init__.py
+++ src/pyg_encoders/__init__.py
@@ -1,4 +1,5 @@
 from pyg_encoders._encode import encode, decode, dumps, loads, pd2bson, bson2pd, bson2np
+from pyg_encoders._dump import dump, load
 from pyg_encoders._encoders import cell_root, root_path, root_path_check, pd_to_csv, pd_read_csv, \
         pickle_dump, pickle_load, npy_encode, npy_write, parquet_encode, parquet_write, pickle_write, \
         pickle_encode, csv_encode, csv_write, encode, dictable_decode, dictable_decoded
